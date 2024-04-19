ead987cd2dac150761fd5b2bf5690f11286cbad0
Duy Ngo Van <ngovanduy1094@gmail.com>
2024-04-19 22:18:03 +0700

    test_ver1

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="duy_sum_exp",
+    version="0.0.1",
+    author="Ngo Van Duy",
+    author_email="duyngo@example.com",
+    description="Sort description",
+    long_description="Full description",
+    long_description_content_type="text/markdown",
+    url="https://github.com/DuyNgo1094/test_lib_py",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
